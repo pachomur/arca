3f69c2ff99ca1a899d7ff7f582a251c48068d13f
pachomur <pachomur@hotmail.com>
2016-08-18 13:00:24 -0500

    gird gestos, menu desplegable de subcategorias de temas y paginacion de decriptores

--- app/controllers/descriptions_controller.rb
+++ app/controllers/descriptions_controller.rb
@@ -4,7 +4,7 @@ class DescriptionsController < ApplicationController
   # GET /descriptions
   # GET /descriptions.json
   def index
-    @descriptions = Description.all.paginate(page: params[:page], per_page: 50).order('descriptor ASC')
+    @descriptions = Description.all.paginate(page: params[:page], per_page: 50).order('description ASC')
   end
 
   # GET /descriptions/1
