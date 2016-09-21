85d5d1c1fec81b241177ca23e2f755b73d18b248
pachomur <pachomur@hotmail.com>
2016-09-20 22:03:03 -0500

    imagen gesto en modo edición

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,16 +1,12 @@
 class Author < ActiveRecord::Base
 
   has_many :artworks
-  nombre = :basename.to_s.rjust(9, '0')
-  nombre = nombre.scan(/.{5}|.+/).join("/")
   has_attached_file :avatar,
                     :styles =>  {  :large => "500x500>",
                                   :medium => "300x300>",
                                   :thumb => "100x100>"
                                 },
-                    :default_url => ":style/missing.png",
-                    :path => "/system/artworks/avatars/:basename/:style/:basename.:extension",
-                    :url => "/system/artworks/avatars/"+ nombre + "/:style/:basename.:extension"
+                    :default_url => ":style/missing.png"
   validates_attachment_content_type :avatar, :content_type => ["image/jpg", "image/jpeg", "image/gif", "image/png"]
 
   def get_full_name
